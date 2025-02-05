254e963e8c6a87bd6e66ceaeca3b8978b7e5faac
seung jun <sbs2469@naver.com>
2025-02-05 12:06:28 +0900

    day13 : 자바스크립트(for loop+조건문)

--- Day13/loop-1.js
+++ Day13/loop-1.js
@@ -4,6 +4,12 @@ let sum = 0
 let numBer = parseInt(prompt("몇까지 더할까요?"))
 
 for(i = 1; i < numBer+1 ;i ++) {
+    if(i % 3 === 0){
+        document.write(`<span class="red">${i}</span><br>`)
+        
+    } else {
+        document.write(`${i} <br>`)
+    }
     sum += i
 }
 document.write(`<h2>1~${numBer}까지의 합은 ${sum}입니다</h2>`)
