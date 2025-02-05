69f74fe24b493e861dcc5c3e7f698295606d24e7
seung jun <sbs2469@naver.com>
2025-02-05 17:14:20 +0900

    day13 : 자바스크립트(while loop)

---
+++ Day13/loop-2.js
@@ -0,0 +1,16 @@
+// while loop
+
+// let i = 1
+// let sum = 1
+// while(i<10){
+//     sum *= i
+//     i++;
+// }
+// console.log(sum)
+
+let stars = parseInt(prompt("별을 몇개 표시할까요?"))
+
+while (stars > 0){
+    document.write("❤");
+    stars--;
+}
