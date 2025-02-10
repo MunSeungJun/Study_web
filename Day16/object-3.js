a03c99d21e6a1d96670f8aa8d546cae1b5094442
seung jun <sbs2469@naver.com>
2025-02-10 17:59:38 +0900

    day16 : 자바스크립트(Array객체)

---
+++ Day16/object-3.js
@@ -0,0 +1,16 @@
+// Array()
+
+// let numbers = ["one", "two", "three", "four"]
+
+// for (let i = 0; i < numbers.length; i++){
+//     console.log(numbers[i])
+// }
+
+//연락처 앱
+let phoneBook = [];
+
+phoneBook.push({
+    name: "홍길동",
+    phohe: "",
+    email: "qwe@naver.com"
+})
